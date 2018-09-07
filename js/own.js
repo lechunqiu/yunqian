11bbfaa0d93dc73fa64fbdb573e298a285ffd2ad
lechunqiu <34645309+lechunqiu@users.noreply.github.com>
2018-09-07 20:27:39 +0800

    手机端适配
    机端适配

--- js/own.js
+++ js/own.js
@@ -1,17 +1,17 @@
 $(document).ready(function(){
 	$('.nav_menu').click(function(){
-		$('.top_bar_mob').hide(800)
-		$('.mob_nav').show(800)
+		$('.top_bar_mob').slideUp(800)
+		$('.mob_nav').slideDown(800)
 		$('.mob_zg').show()
 	})
 	$('.close_mob_nav').click(function(){
-		$('.top_bar_mob').show(800)
-		$('.mob_nav').hide(800)
+		$('.top_bar_mob').slideDown(800)
+		$('.mob_nav').slideUp(800)
 		$('.mob_zg').hide()
 	})
 	$('.mob_zg').click(function(){
-		$('.top_bar_mob').show(800)
-		$('.mob_nav').hide(800)
+		$('.top_bar_mob').slideDown(800)
+		$('.mob_nav').slideUp(800)
 		$('.mob_zg').hide()
 	})
     
